d02a4c499352228839e32081ddc421d416a50f57
SISU <atgsisu@gmail.com>
2024-09-13 15:28:42 +0900

    fix(Lotto): Constructor Arguments로 로또 번호를 넘겨받아 생성하도록 코드 수정

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,19 +1,29 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Lotto {
     private List<Integer> numbers;
 
-    public Lotto() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        validate(numbers);
+    public Lotto(List<Integer> numbers) {
+//        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        validateArgumentIsNotOversized(numbers);
+        validateNumbersAreUnique(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateArgumentIsNotOversized(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+    private void validateNumbersAreUnique(List<Integer> numbers) {
+        Set<Integer> numbersSet = new HashSet<>(numbers);
+        if (numbersSet.size() != numbers.size()) {
             throw new IllegalArgumentException();
         }
     }
