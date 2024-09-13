278724a54026696ea8c775ef4c5f1914ad9303c9
SISU <atgsisu@gmail.com>
2024-09-13 14:44:55 +0900

    feat(Deposit): 로또 구입 금액을 입력받아 저장하는 기능 구현

---
+++ src/main/java/lotto/model/Deposit.java
@@ -0,0 +1,13 @@
+package lotto.model;
+
+public class Deposit {
+    int value;
+
+    public Deposit(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
