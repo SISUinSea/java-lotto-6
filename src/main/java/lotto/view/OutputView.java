eb4eafdd3381b113d84f7b55c753c3e70fe4c248
SISU <101076058+SISUinSea@users.noreply.github.com>
2024-09-22 07:53:39 +0000

    fix(OutputView): 출력 형시게 맞도록 띄어쓰기 삭제

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -32,6 +32,6 @@ public class OutputView {
         System.out.println("5개 일치 (1,500,000원) - " + rank[3] + "개");
         System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + rank[2] + "개");
         System.out.println("6개 일치 (2,000,000,000원) - " + rank[1] + "개");
-        System.out.println("총 수익률은 " + rateOfReturn + "%입니다.");
+        System.out.printf("총 수익률은 %.1f%%입니다.", rateOfReturn);
     }
 }
